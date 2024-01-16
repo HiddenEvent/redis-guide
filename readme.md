a9e70d21ef52adcf4f7017bcda4c78cf224a2b88
RichardKim <hiddeneventor@gmail.com>
2024-01-16 21:38:44 +0900

    [String Type] 기본 에제 및 pipeline 명령 배치 처리

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Docker-Redis 명령어
+docker - redis 모니터링 명령어 (실시간 로그 확인)
+```shell
+docker exec -it 34dfed9 redis-cli monitor
+```
+
+docker - redis 접속 명령어
+```shell    
+docker exec -it 34dfed9 redis-cli
+```
--- src/main/java/org/example/redis/StringExam.java
+++ src/main/java/org/example/redis/StringExam.java
@@ -2,14 +2,50 @@ package org.example.redis;
 
 import redis.clients.jedis.JedisPool;
 
+import java.util.List;
+
 public class StringExam {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         try (var jedisPool = new JedisPool("127.0.0.1", 6379)) {
             try (var jedis = jedisPool.getResource()) {
                 jedis.set("users:300:email", "ricky@aa.com");
-                var value = jedis.get("users:300:email");
-                System.out.println(value);
+                jedis.set("users:300:name", "kim");
+                jedis.set("users:300:age", "32");
+
+                /*get => 단일 키를 조회하는 명령어*/
+                var email = jedis.get("users:300:email");
+                System.out.println(email);
+
+                /*mget => 여러개의 키를 동시에 조회하는 명령어*/
+                List<String> UserInfo = jedis.mget("users:300:email", "users:300:name", "users:300:age");
+                UserInfo.forEach(System.out::println);
+
+                /*incr => 숫자를 1씩 증가시키는 명령어*/
+                long counter = jedis.incr("counter");
+                System.out.println(counter);
+
+                /*incrBy => 숫자를 지정한 만큼 증가시키는 명령어*/
+                long counter2 = jedis.incrBy("counter", 10L);
+                System.out.println(counter2);
+
+                /*decr => 숫자를 1씩 감소시키는 명령어*/
+                long counter3 = jedis.decr("counter");
+                System.out.println(counter3);
+
+                /*decrBy => 숫자를 지정한 만큼 감소시키는 명령어*/
+                long counter4 = jedis.decrBy("counter", 20L);
+                System.out.println(counter4);
+
+                /*pipeline=> 여러개의 명령어를 한번에 실행하는 명령어
+                 (pipeline을 사용하면 네트워크 비용을 줄일 수 있다.)*/
+                var pipeline = jedis.pipelined();
+                pipeline.set("users:400:email", "400@aa.com");
+                pipeline.set("users:400:name", "kim 4");
+                pipeline.set("users:400:age", "40");
+                List<Object> objects = pipeline.syncAndReturnAll();
+                objects.forEach(System.out::println);
+
             }
         }
     }
